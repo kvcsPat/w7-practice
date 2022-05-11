b5d7c0ec42d0d4553d4476bd635751697b552c41
kvcsPat <smth.patrik@gmail.com>
2022-05-11 09:49:13 +0200

    data.js added

---
+++ frontend/data.js
@@ -0,0 +1,79 @@
+const beers = {
+	id: "beers",
+	logo: "Best Beers",
+	button: "details",
+	cards: [
+		{
+			title: "Mango Bay",
+			sub: "Mad Scientist Beer",
+			text: "Pale Ale - American"
+		},
+		{
+			title: "Távoli Galaxis",
+			sub: "Rothbeer Brewery",
+			text: "IPA - American"
+		},
+		{
+			title: "Flying Rabbit AIPA",
+			sub: "MONYO Brewing Co.",
+			text: "IPA - American"
+		},
+		{
+			title: "Liquid Cocaine",
+			sub: "Mad Scientist Beer",
+			text: "IPA - Imperial"
+		},
+		{
+			title: "Organic Chocolate Stout",
+			sub: "Samuel Smith Old Brewery",
+			text: "Stout - English"
+		},
+		{
+			title: "Bracia",
+			sub: "Thornbridge Brewery",
+			text: "Strong Ale - English"
+		},
+		{
+			title: "Oatmeal Stout",
+			sub: "Samuel Smith Old Brewery",
+			text: "Stout - Oatmeal"
+		},
+		{
+			title: "Black Tokyo Horizon",
+			sub: "BrewDog",
+			text: "Stout - American Imperial"
+		},
+		{
+			title: "Vintage Ale",
+			sub: "Fuller's",
+			text: "Old Ale"
+		},
+		{
+			title: "All the Leaves are Brown",
+			sub: "Tempest Brewing Company",
+			text: "Brown Ale - American"
+		},
+	]
+}
+
+function maxMultiple(divisor, bound){
+	let max = 0;
+
+	for (i = bound; i > 0; i--) {
+	  if (i % divisor === 0) {
+		max = i;
+	  }
+	}
+	return max;
+  }
+
+  function maxMultiple(divisor, bound){
+	let max = 0;
+	
+	for (i = divisor; i <= bound; i++) {
+	  if ( i % divisor === 0) {
+		max = i
+	  }
+	}
+	return max
+  }
